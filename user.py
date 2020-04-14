03d9c7f3eee4d2899a6d2ca230a03619a567fbdf
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-04-14 10:18:50 +0300

    created constructor and passed in 4 arguments

--- user.py
+++ user.py
@@ -2,3 +2,11 @@ class User:
     """
     Class that generates instances of users 
     """
+    
+    def __init__(self, first_name, last_name, password):
+        '''
+        __init__ method that helps us define properties for our objects
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password    
