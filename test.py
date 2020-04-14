17570c34d08338369445204d5d315380328afd43
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-04-14 11:06:48 +0300

    created a test_save_user that tests if user is saved into user list

--- test.py
+++ test.py
@@ -24,9 +24,18 @@ class TestUser(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_contact.first_name,"Dennis")
-        self.assertEqual(self.new_contact.last_name,"Mwaniki")
-        self.assertEqual(self.new_contact.password,"Mwaniki91")
+        self.assertEqual(self.new_user.first_name,"Dennis")
+        self.assertEqual(self.new_user.last_name,"Mwaniki")
+        self.assertEqual(self.new_user.password,"Mwaniki91")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new contact
+        self.assertEqual(len(User.user_list),1)
+    
 
 
 if __name__ == '__main__':
