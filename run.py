8c6ae266de357d5eb018f1d697db2ba1a1401d66
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-04-18 12:11:01 +0300

    created a function to create new user account in run.py

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3.6
 import pyperclip
 from test import User, Credential
+
+def create_user(fname, lname, password):
+    '''
+    Function that creates a new user account.
+    '''
+    new_user = User(fname, lname, password)
+    return new_user
